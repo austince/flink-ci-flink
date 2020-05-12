404697d6929604c686d252659abc5e1af3372593
Aljoscha Krettek <aljoscha@apache.org>
2020-05-12 18:20:02 +0200

    [FLINK-17658] Remove Serializable from TimestampAssigner interface
    Instead we introduce SerializableTimestampAssigner to still allow users
    to use lambdas or anonymous inner classes in the API.

--- flink-core/src/main/java/org/apache/flink/api/common/eventtime/SerializableTimestampAssigner.java
+++ flink-core/src/main/java/org/apache/flink/api/common/eventtime/SerializableTimestampAssigner.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,16 +20,11 @@ package org.apache.flink.api.common.eventtime;
 
 import org.apache.flink.annotation.PublicEvolving;
 
+import java.io.Serializable;
+
 /**
- * A {@link TimestampAssigner} that simply forwards the internal timestamp.
+ * A {@link TimestampAssigner} that is also {@link java.io.Serializable}.
  */
 @PublicEvolving
-public final class RecordTimestampAssigner<E> implements SerializableTimestampAssigner<E> {
-
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	public long extractTimestamp(E element, long recordTimestamp) {
-		return recordTimestamp;
-	}
-}
+@FunctionalInterface
+public interface SerializableTimestampAssigner<T> extends TimestampAssigner<T>, Serializable {}
